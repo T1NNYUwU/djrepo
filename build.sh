9e5643ef3efd3945b3e058a88f835dc8811aef40
T1NNY <tinnapat.takananant@gmail.com>
2026-03-30 23:10:23 +0700

    .

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 # build.sh (ตรวจสอบว่ามีไฟล์นี้อยู่ในโปรเจกต์)
+
 #!/usr/bin/env bash
+
 # Exit on error
+
 set -o errexit
+
+
 # ติดตั้ง Dependencies
-export PYTHONPATH=$PYTHONPATH:$(pwd)/backend
 
 pip install -r requirements.txt
+
+
 # รวบรวม Static Files (CSS, JS, Images)
+
 python manage.py collectstatic --no-input
+
+
 # อัปเดตโครงสร้างฐานข้อมูล
+
 python manage.py migrate
